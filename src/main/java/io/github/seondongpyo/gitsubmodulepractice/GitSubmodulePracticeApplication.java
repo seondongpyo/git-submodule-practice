1eda3bfab95034660c415169becff961f1525508
seondongpyo <seondongpyo@gmail.com>
2021-08-01 11:03:25 +0900

    initial commit

---
+++ src/main/java/io/github/seondongpyo/gitsubmodulepractice/GitSubmodulePracticeApplication.java
@@ -0,0 +1,13 @@
+package io.github.seondongpyo.gitsubmodulepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSubmodulePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSubmodulePracticeApplication.class, args);
+	}
+
+}
